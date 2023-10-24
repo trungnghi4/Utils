fd834dc99fdc6d3db03f8e11a3ea754e479443f9
nghiant <nghiant>
2023-10-24 09:38:40 +0700

    [+] Add another fun compare version

--- compare_version_app.kt
+++ compare_version_app.kt
@@ -12,3 +12,18 @@ private fun versionCompare(v1: String, v2: String): Int {
     }
     return 0
 }
+
+fun versionCompare2(v1: String, v2: String): Int {
+    if (v1.isEmpty() || v2.isEmpty()) return 0
+    val version1 = v1.split(".").map { it.toInt() }
+    val version2 = v2.split(".").map { it.toInt() }
+    val length = maxOf(version1.size, version2.size)
+    for (i in 0 until length) {
+        val num1 = if (i < version1.size) version1[i] else 0
+        val num2 = if (i < version2.size) version2[i] else 0
+        if (num1 > num2) return 1
+        if (num2 > num1) return -1
+    }
+    return 0
+}
+
